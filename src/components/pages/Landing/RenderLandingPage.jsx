f9d29b4d157633c7787ecd5303d6e619075a94d4
joe b <j.boucher690@gmail.com>
2024-03-05 10:30:49 -0500

    ticket 1 complete

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -33,6 +33,38 @@ function RenderLandingPage(props) {
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
       {/* <div className="graphs-section"> */}
+
+      <div className="graphs-section">
+        <div className="graphs-container">
+          <div className="graph-item">
+            <img
+              src={GrantRatesByOfficeImg}
+              alt="Asylum Office Grant Rates by Office"
+              className="grants-rates-by-office-graph-img"
+            />
+            <h2 className="image-caption">Search Grant Rates By Office</h2>
+          </div>
+
+          <div className="graph-item">
+            <img
+              src={GrantRatesByNationalityImg}
+              alt="Asylum Office Grant Rates by Nationality"
+              className="grants-rates-by-nationality-graph-img"
+            />
+            <h2 className="image-caption">Search Grant Rates By Nationality</h2>
+          </div>
+
+          <div className="graph-item">
+            <img
+              src={GrantRatesOverTimeImg}
+              alt="Asylum Office Grant Rates Over Time"
+              className="grants-rates-over-time-graph-img"
+            />
+            <h2 className="image-caption">Search Grant Rates Over Time</h2>
+          </div>
+        </div>
+      </div>
+
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -62,6 +94,36 @@ function RenderLandingPage(props) {
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
         {/* <div className="bottom-section">*/}
+
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div className="data-container">
+            <div className="first-data-container">
+              <h2>36%</h2>
+              <h3>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </h3>
+            </div>
+            <div className="second-data-container">
+              <h2>5%</h2>
+              <h3>
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </h3>
+            </div>
+            <div className="third-data-container">
+              <h2>6x Lower</h2>
+              <h3>
+                Between fiscal year 2017 and 2020, the New York asylum office’s
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </h3>
+            </div>
+          </div>
+        </div>
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
